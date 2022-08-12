dc672710a8401fcda391294ba091fe6bc2a36dc2
Sailor4456 <info.vipul1201@gmail.com>
2022-08-12 11:23:13 +0530

    init commit 2

---
+++ HTML/Tasks/firstModule/main.js
@@ -0,0 +1,21 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+  <h1>Module ONE - HTML</h1>
+    <a href="exeOne.html">
+      <img src="https://www.kidsmathgamesonline.com/images/pictures/numbers600/number1.jpg" alt="Practice ONE from module ONE" class="logo" alt="Vite logo" />
+    </a>
+    <a href="exeTwo.html">
+      <img src="https://www.pngall.com/wp-content/uploads/2/2-Number-PNG-Pic.png" alt="Practice TWO from module ONE" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <a href="exeThree.html">
+      <img src="https://cdn.pixabay.com/photo/2017/04/12/01/04/alphabet-2223312_960_720.png" alt="Assignment ONE from module ONE" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Practice Exercises and Assignments...</h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
