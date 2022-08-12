4ac7956837f5e5b99e3291174d41b330efe537d9
Sailor4456 <info.vipul1201@gmail.com>
2022-08-12 14:43:34 +0530

    module one updated

--- HTML/Tasks/firstModule/main.js
+++ HTML/Tasks/firstModule/main.js
@@ -9,7 +9,7 @@ document.querySelector('#app').innerHTML = `
     <a href="exeTwo.html">
       <img src="https://www.pngall.com/wp-content/uploads/2/2-Number-PNG-Pic.png" alt="Practice TWO from module ONE" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <a href="exeThree.html">
+    <a href="assign1Form.html">
       <img src="https://cdn.pixabay.com/photo/2017/04/12/01/04/alphabet-2223312_960_720.png" alt="Assignment ONE from module ONE" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Practice Exercises and Assignments...</h1>
