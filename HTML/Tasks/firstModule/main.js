d6d3cd8da18cdbfbfbedcb8915d89450d4197dc2
Sailor4456 <info.vipul1201@gmail.com>
2022-08-12 19:06:04 +0530

    resume intial commit

--- HTML/Tasks/firstModule/main.js
+++ HTML/Tasks/firstModule/main.js
@@ -4,13 +4,16 @@ document.querySelector('#app').innerHTML = `
   <div>
   <h1>Module ONE - HTML</h1>
     <a href="exeOne.html">
-      <img src="https://www.kidsmathgamesonline.com/images/pictures/numbers600/number1.jpg" alt="Practice ONE from module ONE" class="logo" alt="Vite logo" />
+      <img src="p1.png" alt="Practice ONE from module ONE" class="logo" alt="Vite logo" />
     </a>
     <a href="exeTwo.html">
-      <img src="https://www.pngall.com/wp-content/uploads/2/2-Number-PNG-Pic.png" alt="Practice TWO from module ONE" class="logo vanilla" alt="JavaScript logo" />
+      <img src="p2.png" alt="Practice TWO from module ONE" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <a href="assign1Form.html">
-      <img src="https://cdn.pixabay.com/photo/2017/04/12/01/04/alphabet-2223312_960_720.png" alt="Assignment ONE from module ONE" class="logo vanilla" alt="JavaScript logo" />
+      <img src="a1.png" alt="Assignment ONE from module ONE" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <a href="resume.html">
+    <img src="a2.png" alt="Assignment ONE from module TWO" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Practice Exercises and Assignments...</h1>
   </div>
