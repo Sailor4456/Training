5c3e6f3947bc31c53be7796d46202cda6496a115
Sailor4456 <info.vipul1201@gmail.com>
2022-08-19 11:12:56 +0530

    sonarBugUpdate

--- Tasks/cssM2.js
+++ Tasks/cssM2.js
@@ -2,12 +2,18 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
-  <h1>Module TWO - CSS</h1>
-    <a href="/CSS/alignReportCard.html">
-      <img src="/HTML/public/p1.png" alt="Practice ONE from module ONE" class="logo" alt="Vite logo" />
+  <h1>Module ONE - CSS</h1>
+    <a href="/CSS/threeDiv.html">
+      <img src="/public/p1.png" alt="Practice ONE from module ONE" class="logo" alt="Vite logo" />
     </a>
-    <a href="/CSS/paragraph.html">
-      <img src="/HTML/public/p2.png" alt="Practice TWO from module ONE" class="logo" alt="Vite logo" />
+    <a href="/CSS/positionProperties.html">
+      <img src="/public/p2.png" alt="Practice TWO from module ONE" class="logo" alt="Vite logo" />
+    </a>
+    <a href="/CSS/overflowProperties.html">
+      <img src="/public/p3.png" alt="Practice TWO from module ONE" class="logo" alt="Vite logo" />
+    </a>
+    <a href="/CSS/basicLayout.html">
+      <img src="/public/a1.png" alt="Assignment ONE from module ONE" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Practice Exercises and Assignments...</h1>
   </div>
